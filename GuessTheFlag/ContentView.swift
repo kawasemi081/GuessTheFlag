0eee716b04202d8a1b5bf10baf006e71b89e0f22
midori2012 <misono5861@gmail.com>
2019-10-24 16:30:01 +0900

    Day20 practice

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//
+//  Created on 2019/10/24.
+//  Copyright © 2019 MidoriISONO. All rights reserved.
+//  Day21 Text: https://www.hackingwithswift.com/100/swiftui/21
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
